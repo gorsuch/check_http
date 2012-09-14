d3d4fd338ac00e934cfaeb8b007ce43b2493441f
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-09-14 10:32:18 -0700

    specs for Checker

--- lib/check_http/checker.rb
+++ lib/check_http/checker.rb
@@ -1,15 +1,29 @@
 module CheckHTTP
   class Checker
-    def blacklist
+    attr_accessor :blacklist
+
+    def initialize
+      self.blacklist = default_blacklist
+    end
+
+    def default_blacklist
       [:response_header, :response_body, :httpauth_avail, :starttransfer_time, :appconnect_time, :pretransfer_time]  
     end
 
     def check(url)
+      clean(sample(url)).merge(:url => url)
+    end
+
+    def clean(result)
+      result.reject { |k,v| blacklist.include?(k) }
+    end
+
+    def sample(url)
       ethon = Ethon::Easy.new
       ethon.url = url
       ethon.prepare
       ethon.perform
-      ethon.to_hash.reject { |k,v| blacklist.include?(k) }.merge(:url => url)
+      ethon.to_hash
     end
   end
 end
